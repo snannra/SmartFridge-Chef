f6926e33d1026fffa0471c303754927a14692df9
Sohan Nannra <49798755+snannra@users.noreply.github.com>
2024-06-22 23:10:39 -0700

    editing app.js

--- SmartFridgeChefMobile/src/App.js
+++ SmartFridgeChefMobile/src/App.js
@@ -1,30 +1,3 @@
-// src/App.js
-
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './screens/HomeScreen';
-import RecipeScreen from './screens/RecipeScreen';
-
-const Stack = createStackNavigator();
-
-const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: 'SmartFridge Chef' }}
-        />
-        <Stack.Screen
-          name="Recipe"
-          component={RecipeScreen}
-          options={{ title: 'Recipe Details' }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
-
-export default App;
